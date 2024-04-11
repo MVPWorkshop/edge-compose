f556f4582fe72f022b565081b46641146a80182b
djuromatic <djuromatic991@gmail.com>
2024-04-11 12:32:41 +0200

    fixing path

--- genesis-init.sh
+++ genesis-init.sh
@@ -4,6 +4,7 @@ set -ex
 PUBLIC_IP=$(curl ifconfig.io)
 NODE_ID=$(cat secrets-output.json | jq -r '.[0] | .node_id')
 BOOTNODE=/ip4/${PUBLIC_IP}/tcp/1478/p2p/${NODE_ID}
+BOOTNODELOCAL=/ip4/127.0.0.1/tcp/1478/p2p/${NODE_ID}
 PREMINE=0xae1122329CC1ed0Ab344DB182e36939Bf3AdDAc3:0x1431e0fae6d7217caa0000000
 CHAIN_ID=33221
 
@@ -17,7 +18,6 @@ if [ ! -f secrets-output.json ]; then
 	echo "Please run the setup.sh script first."
 	exit 1
 fi
-
 if [ ! -f genesis.json ]; then
 	polygon-edge genesis --ibft-validator-type ecdsa --consensus ibft --ibft-validators-prefix-path validator- --bootnode ${BOOTNODE} --premine ${PREMINE} --chain-id ${CHAIN_ID}
 fi
