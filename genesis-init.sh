912b3f5c32cbc3fd3384a540a4ecf767c956f3f3
djuromatic <djuromatic991@gmail.com>
2024-04-11 11:00:59 +0200

    add custom chain id to init script

--- genesis-init.sh
+++ genesis-init.sh
@@ -5,6 +5,7 @@ PUBLIC_IP=$(curl ifconfig.io)
 NODE_ID=$(cat secrets-output.json | jq -r '.[0] | .node_id')
 BOOTNODE=/ip4/${PUBLIC_IP}/tcp/1478/p2p/${NODE_ID}
 PREMINE=0xae1122329CC1ed0Ab344DB182e36939Bf3AdDAc3:0x1431e0fae6d7217caa0000000
+CHAIN_ID=33221
 
 if (! command -v jq &>/dev/null); then
 	echo "jq is not installed. Please install jq."
@@ -18,6 +19,6 @@ if [ ! -f secrets-output.json ]; then
 fi
 
 if [ ! -f genesis.json ]; then
-	polygon-edge genesis --ibft-validator-type ecdsa --consensus ibft --ibft-validators-prefix-path validator- --bootnode ${BOOTNODE} --premine ${PREMINE}
+	polygon-edge genesis --ibft-validator-type ecdsa --consensus ibft --ibft-validators-prefix-path validator- --bootnode ${BOOTNODE} --premine ${PREMINE} --chain-id ${CHAIN_ID}
 fi
 exit 0
